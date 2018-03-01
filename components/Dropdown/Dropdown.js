025d7ee47416ebf10aaf4764b0a3cc54d7fd6be6
unknown <luisd824@gmail.com>
2018-03-01 13:57:43 -0500

    better css styling on boxes

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,13 +1,15 @@
-
 let links = document.getElementsByClassName('Dropdown')[0];
 let hidden = document.getElementById('Dropdown__hide');
 
+
 links.addEventListener('click', () => {
+
     if(hidden.style.display === 'none') {
         hidden.style.display = 'block';
     } else {
         hidden.style.display = 'none';
     }
+    
 });
 
 
