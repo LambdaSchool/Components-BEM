b2b173756ac147218b59cf4670d86f8e404a623b
Kenny <khpham24@gmail.com>
2018-02-28 23:23:56 -0700

    Got drop down to work

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,21 +1,26 @@
 // Does not work yet
 
-class DropDownItem {
-    constructor(element) {
-        this.element = element;
-        this.button = document.getElementsByClassName('Dropdown__button');
-        this.button.addEventListener('click', () => {
-            this.dropDownMenu();
-        });
-    }
+// class DropDownItem {
+//     constructor(element) {
+//         this.element = element;
+//         this.button = this.element.getElementsByClassName("Dropdown__button");
+//         this.button.addEventListener("click", () => {
+//             this.dropDownMenu();
+//         });
+//     }
 
-    dropDownMenu() {
-        const element = this.element.classList.click('Dropdown__link');
-    }
-}
+//     dropDownMenu() {
+//         this.element.classList.toggle("show");
+//     }
+// }
 
-let toggles = document.getElementsByClassName('Dropdown');
-toggles = Array.from(toggles).map(toggle => {
-    return new DropDownItem(toggle);
-});
+// let toggles = document.getElementsByClassName("Dropdown");
+// toggles = Array.from(toggles).map(toggle => {
+//     return new DropDownItem(toggle);
+// });
 
+
+
+
+  const drop = () => {
+    document.getElementById("Dropdown__menu").classList.toggle("show");}
