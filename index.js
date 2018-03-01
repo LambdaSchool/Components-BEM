395d1cbc89da1828bbd03ce2712aaca4a201d006
dc5stars <dc5stars@yahoo.com>
2018-03-01 12:57:49 -0500

    commit attempt

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-class Solution {
+class Dropdown {
     constructor(element) {
       this.element = element;
   
       console.log(this.element)
-  
+      //msg = <ul>
+      
       this.element.addEventListener("click", () => {
-      this.element.classList.add("Block--hidden");
+      this.element.classList.toggle("selected");
+      //this.element.innerHTML = '<div class="Box"><ul><li>Lambda</li><li>Google</li><li>MDN</li></ul><div>';
+
+
       });
-    }
+
+      
+
+
+
+      //this.element.addEventListener("click", () => {
+      //this.element.classList.remove("selected");
+      //this.element.innerHTML = 'Back to normal';
+      //this.element.innerHTML("Holy Shit it's working!")});
+      //});
   }
+}
+  let topDropdown = document.getElementsByClassName("Dropdown");
+  console.log(topDropdown);
+  topDropdown = new Dropdown(topDropdown[0]);
   
-  let blocks = document.getElementsByClassName("Block");
-  blocks = Array.from(blocks).map(block => {
-    return new Block(block)
-  });
+  //topDropdown = 
   
-  console.log(blocks);
+  //blocks = Array.from(blocks).map(block => {
+    //return new Block(block)
+  //});
+  
+  
