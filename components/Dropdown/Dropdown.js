1873fa85df8ad312ebe6b46692f626c1be291c63
Alexander <alexander.botello@g.austincc.edu>
2018-03-14 18:57:07 -0500

    Use ternary statement in method logic

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -3,18 +3,15 @@ class Dropdown {
         this.menu = element;
         this.button = document.querySelector('.Dropdown')
         this.button.addEventListener("click", () => {
-            this.expand();
+            this.expand_menu();
         });
     }
-    expand() {
-        if (this.menu.style.display === 'none') {
-            this.menu.style.display = 'flex';
-        } else {
-            this.menu.style.display = 'none';
-        }
+    expand_menu() {
+        this.menu.style.display === 'none' 
+            ? this.menu.style.display = 'flex' 
+            : this.menu.style.display = 'none';
     }
 }
 
 let menu = document.querySelector(".Dropdown__menu");
-
 const dropdown = new Dropdown(menu);
