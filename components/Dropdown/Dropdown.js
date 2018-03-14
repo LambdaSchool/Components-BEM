66587ff656af3fb9128135bc506d4cde60388c3b
Alexander <alexander.botello@g.austincc.edu>
2018-03-13 20:46:18 -0500

    Components-BEM complete

---
+++ components/Dropdown/Dropdown.js
@@ -0,0 +1,20 @@
+class Dropdown {
+    constructor(element) {
+        this.menu = element;
+        this.button = document.querySelector('.Dropdown')
+        this.button.addEventListener("click", () => {
+            this.expand();
+        });
+    }
+    expand() {
+        if (this.menu.style.display === 'none') {
+            this.menu.style.display = 'flex';
+        } else {
+            this.menu.style.display = 'none';
+        }
+    }
+}
+
+let menu = document.querySelector(".Dropdown__menu");
+
+const dropdown = new Dropdown(menu);
