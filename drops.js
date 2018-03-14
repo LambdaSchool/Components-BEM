2fef88e701a8538916aabfca30fbc0472d0462cc
Charlie <ccariste@gmail.com>
2018-03-13 19:35:18 -0700

    CRC: DD still not working, changing classes instead

--- drops.js
+++ drops.js
@@ -2,12 +2,14 @@ class Dropdown {
     constructor(element) {
         this.element = element;
         this.element.addEventListener('click', () => {
-            this.element.classList.add('Dropdown__Content--Display')
+            let dropContent = document.getElementsByClassName('Dropdown__Content'); 
+            // dropContent.classList.add('Dropdown__Content--Display');
+            console.log(dropContent);
         });
     }
 }
 
-let dropdown = document.getElementsByClassName('Dropdown__Content');
+let dropdown = document.getElementsByClassName('Dropdown');
 dropdown = Array.from(dropdown).map(dd => {
-    return new Dropdown(dropdown)
+    return new Dropdown(dd);
 });
