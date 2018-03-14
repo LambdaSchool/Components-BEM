55f5f4cdc5bebcd606687e30d19ebc3a89a406b9
Andrew Jarrett <ahrjarrett@gmail.com>
2018-03-13 22:02:09 -0600

    refactor Dropdown.js to remove unused event parameter from callbacks

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -4,11 +4,11 @@ class Dropdown {
     this.clicked = false
     this.child = [...this.element.children].filter(child => child.className === hideClass)[0]
 
-    this.element.addEventListener('click', (e) => this.handleClickEvent(e))
+    this.element.addEventListener('click', () => this.handleClickEvent())
   }
 
-  handleClickEvent(e) {
-    if (this.clicked && this.child.classList.length) this.child.classList.remove('display')
+  handleClickEvent() {
+    if (this.clicked && this.child.classList) this.child.classList.remove('display')
     else this.child.classList.add('display')
     this.toggleDropdown()
   }
