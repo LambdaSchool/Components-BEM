41d754d2d299a55e559a448135c11e7591c3a505
Charlie <ccariste@gmail.com>
2018-03-14 15:55:17 -0700

    CRC: Add drop down click away event listener and toggle on off

--- drops.js
+++ drops.js
@@ -1,29 +1,27 @@
 class Dropdown {
     constructor(element) {
         this.element = element;
+        console.log('this', this);
         this.child = document.querySelector('.Dropdown__Content');
-        console.log(this.child.classList);
-        this.clicked = false;
         this.element.addEventListener('click', () => {
             // let children = this.element.children;
             // children[1].classList.add('Dropdown--Display'); // for use with getElementsByClassName array
-            this.child.classList.add('Dropdown--Display');
-            this.clicked = true;
-            clicked();
+            this.child.classList.toggle('Dropdown--Display');
         });
-    }
-    
-    clicked() {
-        if (this.clicked) {
-            console.log(true);
-            this.element.addEventListener('click', () => {
-                this.child.classList.remove('Dropdown--Display');
-            });
-        }
     }
 }
 
 let dropdown = document.getElementsByClassName('Dropdown');
 dropdown = Array.from(dropdown).map(dd => {
     return new Dropdown(dd);
+});
+
+let secLower = document.querySelector('.Section--Lower');
+secLower.addEventListener('click', () => {
+    let ddContent = document.querySelector('.Dropdown--Display');
+    if (ddContent) {
+        console.log(true);
+        ddContent.classList.remove('Dropdown--Display');
+        console.log(ddContent.classList);
+    }
 });
