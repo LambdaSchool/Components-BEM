048a082447084b79eca9d6dc7b2de172d729362a
Faulkenfett11 <millennium.faulken@gmail.com>
2018-03-28 15:44:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
 
-function myFunction() {
-    document.getElementById("dropstuff").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.Dropdown__menu')) {
-
-    var dropdowns = document.getElementsByClassName("Dropdown__items");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
