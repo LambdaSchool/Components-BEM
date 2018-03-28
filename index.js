ac99576911d97e1287fb95fa9c4edf0d47a065d8
totsuya <supertatortots@gmail.com>
2018-03-28 17:33:31 -0400

    moved variables to their own section for later

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const dropdownButton = document.querySelector('.dropdown__button');
+const dropdownMenu = document.querySelector('.dropdown__menu');
+
 class Dropdown {
   constructor() {
-    this.dropdownButton = document.querySelector('.dropdown__button');
-    this.dropdownMenu = document.querySelector('.dropdown__menu');
-    console.log(this.dropdownMenu);
-
-    this.dropdownButton.addEventListener('click', this.revealMenu.bind(this));
-  }
-  revealMenu(event) {
-    this.dropdownMenu.classList.toggle('dropdown__menu--reveal');
   }
 }
+
+
+
+
+// for testing buttons
+// const dropdownButton = document.querySelector('.dropdown__button');
+//
+// const testingstuff = (event) => {
+//   console.log('hello I am a test!')
+// }
+//
+// dropdownButton.addEventListener('click', testingstuff);
