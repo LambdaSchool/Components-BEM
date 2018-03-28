3ce01dcf6a25d563898424db2781a70391bb1182
totsuya <supertatortots@gmail.com>
2018-03-28 18:05:07 -0400

    got dropdown menu to work had to rename css stuff for some reason

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const dropdownButton = document.querySelector('.dropdown__button');
 const dropdownMenu = document.querySelector('.dropdown__menu');
 
 class Dropdown {
-  constructor() {
+  constructor(button, menu) {
+    button.addEventListener('click', (event) => {
+    menu.classList.toggle('dropdownreveal');
+  });
   }
 }
 
-
-
+const dd = new Dropdown(dropdownButton, dropdownMenu);
 
 // for testing buttons
 // const dropdownButton = document.querySelector('.dropdown__button');
