4cf773dbe1f1a28a8745057a9674da4af635b828
jeffrey <jeffreyneilflynn@gmail.com>
2018-03-28 18:53:46 -0400

    removed some files that werent being used

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 class Dropdown {
 	constructor(element) {
 		this.element = element;
-	}
 
-	dropdownOptions() {
 		this.element.addEventListener('click', function() {
 			document.querySelector('.Dropdown__options--click').style.display = 'relative';
 		});
 	}
 
+	// dropdownOptions() {
+	// 	this.addEventListener('click', function() {
+	// 		document.querySelector('.Dropdown__options--click').style.display = 'relative';
+	// 	});
+	// }
+
 }
 
-const address = document.querySelector('.Box__header--dropdown');
+const address = document.querySelector('.Dropdown__button');
 const dropdown = new Dropdown(address);
 
-dropdown.dropdownOption();
+dropdown();
