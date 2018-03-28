eabe9e3fc96e4a4f559e14dd9c0af9604982fee2
Jared Cuffe <jcuffe@gmail.com>
2018-03-28 12:31:07 -0700

    BEM design. Used my own naming scheme as the provided one seemd redundant. Dropdown working!

---
+++ index.js
@@ -0,0 +1,14 @@
+class Dropdown {
+  constructor() {
+    this.dropdownButton = document.querySelector('.dropdown__button');
+    this.dropdownMenu = document.querySelector('.dropdown__menu');
+    console.log(this.dropdownMenu);
+
+    this.dropdownButton.addEventListener('click', this.toggleMenu.bind(this));
+  }
+  toggleMenu(event) {
+    this.dropdownMenu.classList.toggle('dropdown__menu--show');
+  }
+}
+
+window.onload = () => new Dropdown();
