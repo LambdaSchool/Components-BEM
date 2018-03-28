b48e4cca5e417f98509583eb708066370893f832
Justin Hong <jhong2009@gmail.com>
2018-03-28 15:26:08 -0700

    revised drop down area and styled it

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// let blocks = document.querySelectorAll('.Block');
-
-// class Block {
-//   constructor(el){
-//     this.element = el;
-//     this.element.addEventListener('click', () => { this.highlight()})
-//   }
-//   highlight(){
-//     this.element.classList.toggle('Block--redBorder');
-//   }
-// }
-
-// blocks = Array.from(blocks).map( element => {
-//   return new Block(element);
-// })
-
 // access dropDown button
 let dropDown = document.querySelector('.Box__DropdownContent');
 let dropButton = document.querySelector('.Box__DropdownBtn');
 let childDropdown = dropDown.children;
+// console.log(dropDown);
+// console.log(childDropdown);
 
 // Dropdown constructor
 class Dropdown {
@@ -25,18 +11,18 @@ class Dropdown {
 		this.element = el;
 		this.element.addEventListener('click', this.showContent)
 	}
-	// Callback Function
+	// Dropdown prototype
 	showContent() {
 		dropDown.classList.toggle("show");
 	}
 }
 
-// Dropdown prototype
+// make instances of Dropdown
 childDropdown = Array.from(childDropdown).map( element => {
 	return new Dropdown(element);
 })
-console.log(childDropdown);
+// console.log(childDropdown);
 
 for (let i = 0; i < childDropdown.length; i++) {
-	dropButton.addEventListener('click', childDropdown[i].showContent)
+	dropButton.addEventListener('click', childDropdown[i].showContent);
 }
