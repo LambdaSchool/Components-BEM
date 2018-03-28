e0b233ee7642ad46a8a0f8df506076ba3c04c255
Devon Bacon <devon.bacon@gmail.com>
2018-03-28 13:50:15 -0700

    Included functionality of drop down and added some styles

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("connected")
+// Set up references to the DOM
+const dropdown = document.querySelector(".Box__headDropdown");
+const dropUl = document.querySelector(".Box__headDropUl");
+
+dropdown.addEventListener('click', () => {
+  dropUl.classList.toggle('Box__headDropUl');
+});
