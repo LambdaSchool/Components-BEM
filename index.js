203cd729cc117ca003dd5b359d7a57d8d1b7d0b9
sadrak105 <sadrak10@gmail.com>
2018-03-28 17:24:17 -0500

    class dropdown

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 const dropdown = document.querySelector(".Box__headDropdown");
 const dropUl = document.querySelector(".Box__headDropUl");
 
-dropdown.addEventListener('click', () => {
-  dropUl.classList.toggle('Box__headDropUl');
-});
+//Make dropdown component class
+class Dropdown {
+  constructor(elements, trigger) {
+    //console.log(elements);
+    //Expand functionality
+    trigger.addEventListener('click', () => {
+    //console.log(elements);
+      elements.classList.toggle('Box__headDropUl');
+    });
+  }
+}
+//Initialized Event Listener
+new Dropdown(dropUl, dropdown);
