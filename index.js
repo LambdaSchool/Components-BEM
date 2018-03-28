4269800f2f3ed05e2f964b1991a3b86c43a869b8
ajf412 <aaronjfaulkner@yahoo.com>
2018-03-28 19:38:23 -0400

    Still working on the project.  Up and down for kids a lot today.

---
+++ index.js
@@ -0,0 +1,14 @@
+const navButtons = document.getElementsByClassName('.Box__navButtons');
+let navButtonOff = false;
+
+function displayNavButtons() {
+    // navButtonOff = !navButtonOff;
+    // if (!navButtonOff){
+         
+    // }
+    // else navButtons.style.display = 'none';
+}
+
+console.log(navButtons);
+
+//navButtons.style.display = 'flex';
