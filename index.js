7df35ad24ea3c9edea71f1e5b2d8a301f1422c72
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-28 15:02:16 -0600

    Added in the class and constructor for the dropdown

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 let dropdown = document.querySelector('.header__button');
-dropdown.addEventListener('click',dropdownOpen)
 
-
-function dropdownOpen() {
-    //console.log('I am being called!');
-    let list = document.querySelector('.header__list');
-    // list.style.display = 'block';
-    if (list.style.display === "none") {
-        list.style.display = "block";
-    } else {
-        list.style.display = "none";
+class Dropdown {
+    constructor (element) {
+        this.element = element;
+        this.element.addEventListener('click', () => {
+            this.dropdownOpen();
+        })
     }
+    dropdownOpen() {
+        //console.log('I am being called!');
+        let list = document.querySelector('.header__list');
+        // list.style.display = 'block';
+        if (list.style.display === "none") {
+            list.style.display = "block";
+        } else {
+            list.style.display = "none";
+        }
+    };
+    
+}
 
 
 
-};
+
+
+dropdown = new Dropdown(dropdown); // this creates an object 
