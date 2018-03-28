4b629b0ba94b2f3b1544ce7e7b352f742083815a
SyCoDe <SyCoDe@gmail.com>
2018-03-28 16:39:45 -0600

    fnished mobile, working on desktop

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-class Box {
-    constructor(element) {
-        
-    }
+// class Box {
+//     constructor(element) {
 
+//     }
+
+// }
+
+function Toggle() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
