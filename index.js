b2a16eaea91697e6eca2f8015daffd4b2557a41b
tanking <tanking@github.com>
2018-03-28 17:22:25 -0400

    Added dropdown prototype and activated dropdown button

--- index.js
+++ index.js
@@ -15,18 +15,28 @@
 // })
 
 // access dropDown button
-let dropDown = document.querySelector('Box__DropdownBtn');
+let dropDown = document.querySelector('.Box__DropdownContent');
+let dropButton = document.querySelector('.Box__DropdownBtn');
+let childDropdown = dropDown.children;
 
 // Dropdown constructor
 class Dropdown {
 	constructor(el) {
 		this.element = el;
-		this.element.addEventListener('click', () => { this.showContent()})
+		this.element.addEventListener('click', this.showContent)
 	}
 	// Callback Function
 	showContent() {
 		dropDown.classList.toggle("show");
 	}
-	// Dropdown prototype
+}
 
+// Dropdown prototype
+childDropdown = Array.from(childDropdown).map( element => {
+	return new Dropdown(element);
+})
+console.log(childDropdown);
+
+for (let i = 0; i < childDropdown.length; i++) {
+	dropButton.addEventListener('click', childDropdown[i].showContent)
 }
