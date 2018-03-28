6ae899f42713c38a15f7094f92038b693112072b
nickbrenn <nbren@tutanota.com>
2018-03-28 18:11:57 -0400

    Dropdown dont work

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,6 +1,13 @@
-class dropdown {
-    constructor(){
-
+class dropdownButton {
+    constructor(element){
+        this.element = element;
+        console.log('hello!? this element??');
+        this.element.addEventListener('click', () => { this.openDropdown });
     };
-
+    openDropdown(){
+        console.log('hello');
+        document.querySelector('.dropdown__stuff').style.display = 'flex';
+    }
 }
+
+const dropdown = new dropdownButton(document.querySelector('.dropdown__button'));
