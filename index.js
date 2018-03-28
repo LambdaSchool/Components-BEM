e930a1fba25c05fbcb0e992cdbc6f52cffb6860e
BUDDHAPLEX <ericandrade@gmail.com>
2018-03-28 18:52:08 -0400

    finished js and most of mobile view.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
+// class Box {
+//     constructor(element) {
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+//     }
+
+// }
+
+function Toggle() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
     }
   }
+}
 }
