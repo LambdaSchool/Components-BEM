84c8e280119848da66e21834422fea82085c0ce5
unknown <bsa6.23.94@gmail.com>
2018-03-28 18:09:11 -0400

    I think im done

---
+++ index.js
@@ -0,0 +1,13 @@
+class ddmenu {
+    constructor(){
+        this.drop = document.querySelector('.dropdown__button');
+        this.menu = document.querySelector('.dropdown__content');
+        
+        this.drop.addEventListener('click', this.tog.bind(this));
+    }
+    tog() {
+        this.menu.classList.toggle('dropdown__content--display');
+    }
+}
+
+window.onload = () => new ddmenu();
