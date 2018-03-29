229c72e40607ea3e84ea2e4e94ccae0bb9ff66c2
imsdhk <jappleleaf@gmail.com>
2018-03-28 23:56:01 -0400

    Start from scratch

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,18 +1,24 @@
-class Dropdown {
-    constructor(element) {
-      this.element = element;
-      this.button = this.element.querySelector(".Dropdown__button");
+// class Dropdown {
+//     constructor(element) {
+//       this.element = element;
+//       this.button = this.element.querySelector(".Dropdown__button");
   
-      this.button.addEventListener("click", () => {
-        this.toggleContent();
-      });
-    }
+//       this.button.addEventListener("click", () => {
+//         this.toggleContent();
+//       });
+//     }
   
-    toggleContent() {
-      let buttonContent = this.element.querySelector('.Dropdown__content');
-      buttonContent.classList.toggle("Dropdown--active");
-    }
-  }
+//     toggleContent() {
+//       let buttonContent = this.element.querySelector('.Dropdown__content');
+//       buttonContent.classList.toggle("Dropdown--active");
+//     }
+//   }
   
-  let dropdowns = document.querySelectorAll(".Dropdown");
-  dropdowns = Array.from(dropdowns).map(dropdown => new Dropdown(dropdown));
+//   let dropdowns = document.querySelector(".Section__Dropdown");
+
+//   window.onload = () => new Dropdown(dropdowns);
+
+
+let dropdowns = document.querySelector(".Dropdown__button");
+let showHide = document.querySelector('.Dropdown__content');
+window.onload = () => dropdowns.addEventListener('click', () => showHide.classList.toggle('Dropdown--active'));
