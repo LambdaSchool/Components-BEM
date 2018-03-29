2e0156f0bdc39a718b45fca5db5774c103124b5e
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-03-28 19:44:38 -0500

    refactored code into ES6 class syntax

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,6 +1,20 @@
-const btn = document.querySelector('.Dropdown__btn');
-const dropdown = document.querySelector('.Dropdown__list');
+let btn = document.querySelector('.Dropdown__btn');
+let dropdown = document.querySelector('.Dropdown__list');
 
-btn.addEventListener('click', () => {
-    dropdown.classList.toggle('Dropdown--active');
-})
+// btn.addEventListener('click', () => {
+//     dropdown.classList.toggle('Dropdown--active');
+// })
+
+// ES6 Syntax
+class Dropdown {
+    constructor(button, dropdownMenu) {
+        this.button = button;
+        this.dropdownMenu = dropdownMenu
+        this.button.addEventListener('click', () => {this.showDropdown()})
+    }
+    showDropdown() {
+        this.dropdownMenu.classList.toggle('Dropdown--active');
+    }
+}
+
+btn = new Dropdown(btn, dropdown);
