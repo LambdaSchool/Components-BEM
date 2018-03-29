170d138e0863fbc535fddb23cf80404fa5c2060e
Shawn Stewart <s.stewart0220@gmail.com>
2018-03-29 16:53:32 -0500

    Completion of BEM

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const dropdown = document.querySelector('.Dropdown');
 const list = document.querySelector('.Dropdown__list');
 
 function toggleList() {
+  console.log(list.classList);
   list.classList.toggle('Dropdown__list--show');
+  console.log(list.classList);
 }
 
 dropdown.addEventListener('click', toggleList);
