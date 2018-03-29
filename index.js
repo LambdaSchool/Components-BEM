c1677ef0ba484f30e10bbf04fb5ed238d912154d
hilal aissani <hilalaissani@gmail.com>
2018-03-29 01:06:56 -0400

    project 70%

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 let bilal  = document.querySelector('.Dropdown');
-
 let hidenchilds = document.querySelector('.Dropdown__hide');
-
 bilal.addEventListener('click', hideChildsFunction);
-
  function hideChildsFunction(){
-     
-     hidenchilds.classList.remove('Dropdown__hide');
-     hidenchilds.classList.add('Dropdown--show')
+    hidenchilds.classList.remove('Dropdown__hide');
+    hidenchilds.classList.add('Dropdown--show');
  
  }
 
