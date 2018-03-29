48750784e7beebc55050bbf952a730094e142b27
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-03-28 22:13:18 -0700

    Created Components for Dropdown

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 const dropdown = document.querySelector('.Dropdown');
 const reveal = document.querySelector('.DropdownReveal');
 
+// Upon click Dropdown will display HTML Attributes //
+class Dropdown {
+    constructor(button, list) {
+        button.addEventListener('click', (event) => {
+            list.classList.toggle('display');
+        });
+    }
+}
+
+const dd = new Dropdown(dropdown, reveal);
+
