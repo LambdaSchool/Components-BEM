bd9307ab5073a6761937b8c24e5eb8854d896719
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-06 23:41:40 -0400

    Added extra comments

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ let dropButton = document.querySelector('.Box__DropdownBtn');
 let childDropdown = dropDown.children; // stores a tags
 
 // Dropdown constructor
+	// classList: This property is useful to add, remove and toggle CSS classes on an element.
+		// toggle: Toggles between a class name for an element.
+	// Toggles the links from display: block to none
 class Dropdown {
 	constructor(el) {
 		this.element = el;
 	}
 	showContent() {
-		dropDown.classList.toggle("show");
+		dropDown.classList.toggle("show"); 
 	}
 }
 
@@ -19,5 +22,4 @@ childDropdown = Array.from(childDropdown).map(element => {
 })
 
 // Target = dropButton, event = 'click', listener = 'showContent()'
-
 dropButton.addEventListener('click', childDropdown[0].showContent);
