aa93c4982c8ac2e0e28d488090aea64c9146a305
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-06 23:35:41 -0400

    Fixed logic and added comments to index.js

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-// access dropDown button
+// Variables
 let dropDown = document.querySelector('.Box__DropdownContent');
 let dropButton = document.querySelector('.Box__DropdownBtn');
-let childDropdown = dropDown.children;
-// console.log(dropDown);
-// console.log(childDropdown);
+let childDropdown = dropDown.children; // stores a tags
 
 // Dropdown constructor
 class Dropdown {
 	constructor(el) {
 		this.element = el;
-		this.element.addEventListener('click', this.showContent)
 	}
-	// Dropdown prototype
 	showContent() {
 		dropDown.classList.toggle("show");
 	}
 }
 
-// make instances of Dropdown
-childDropdown = Array.from(childDropdown).map( element => {
+// Makes new Dropdown, with the method showContent();
+childDropdown = Array.from(childDropdown).map(element => {
 	return new Dropdown(element);
 })
-// console.log(childDropdown);
 
-for (let i = 0; i < childDropdown.length; i++) {
-	dropButton.addEventListener('click', childDropdown[i].showContent);
-}
+// Target = dropButton, event = 'click', listener = 'showContent()'
+
+dropButton.addEventListener('click', childDropdown[0].showContent);
